dae0057e0fa699f70993f1ddb7a7ebcb6df32f2e
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-06-18 21:07:31 -0400

    Navigation links working, page components built

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
 import Navigation from './Navigation';
+import Header from './Header';
 
 const App = () => {
     return(
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
 
 const Navigation = () => {
+    const [page, setPage] = useState('');
+
+    // render pages based on what user selected
+    const showPage = () => {
+        switch(page) {
+            case 'about':
+                return <About />;
+            case 'portfolio':
+                return <Portfolio />;
+            case 'contact':
+                return <Contact />;
+            default: 
+                return null;
+        }
+    }
+
     return (
-        <nav class="navbar navbar-dark bg-dark p-1 px-2 d-flex justify-content-center">
-        <a class="navbar-brand" href="#about" >About Me</a>  
-        <a class="navbar-brand" href="#work" >Portfolio</a>  
-        <a class="navbar-brand" href="#contact" >Contact</a>  
-        <a class="navbar-brand" href="Sam_Gates_Resume.pdf"  target="_blank">Resume</a> 
-    </nav>
+        <div>
+            <nav className="navbar navbar-dark bg-dark p-1 px-2 d-flex justify-content-center">
+            <a 
+                className={page === 'about' ? 'highlight navbar-brand' : 'navbar-brand'} 
+                href="#about" 
+                onClick={() => setPage('about')}
+            >
+            About Me
+            </a>  
+            <a 
+                className={page === 'portfolio' ? 'highlight navbar-brand' : 'navbar-brand'} href="#portfolio" 
+                onClick={() => setPage('portfolio')}
+            >
+            Portfolio
+            </a>  
+            <a 
+                className={page === 'contact' ? 'highlight navbar-brand' : 'navbar-brand'} href="#contact" 
+                onClick={() => setPage('contact')}
+            >
+            Contact
+            </a>  
+            <a className="navbar-brand" href="Sam_Gates_Resume.pdf"  target="_blank">Resume</a> 
+            </nav>
+        {showPage()}
+        </div>
     );
 }
 
